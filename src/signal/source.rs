5c4e5cc118c51fa1b3f9a17366742f5f2fbed6c0
Peter Hahlweg <peter.github@mailbox.org>
2022-11-17 22:35:47 +0100

    Add on_received callback
    - this can be usefull if something should happened after consumtion

--- src/signal/source.rs
+++ src/signal/source.rs
@@ -5,7 +5,8 @@ use loom::Arc;
 pub struct Source<T:Send> {
     signal: Arc<Signal<T>>,
     store: Option<Box<T>>,
-    is_synced: bool
+    is_synced: bool,
+    on_received: Option<Box<dyn FnMut(&T)>>
 }
 
 // impl Source
@@ -14,7 +15,8 @@ impl<T:Send> Source<T> where T: Clone + Sync + Default {
         Source {
             signal: Arc::new(Signal::new(value)),
             store: Some(Box::new(T::default())),
-            is_synced: false
+            is_synced: false,
+            on_received: None
         }
     }
 
@@ -22,7 +24,8 @@ impl<T:Send> Source<T> where T: Clone + Sync + Default {
         Source {
             signal: Arc::new(Signal::new(value)),
             store: Some(Box::new(T::default())),
-            is_synced: true
+            is_synced: true,
+            on_received: None
         }
     }
 
@@ -31,7 +34,8 @@ impl<T:Send> Source<T> where T: Clone + Sync + Default {
         let state = self.try_sync();
         if state == Ready {
             self.store(signal);
-            self.swap()
+            self.swap();
+            self.call_on_received();
         }
         state
     }
@@ -41,13 +45,14 @@ impl<T:Send> Source<T> where T: Clone + Sync + Default {
         let state = self.try_sync();
         if state == Ready {
             closure(self.store.as_mut().expect("always valid data"));
-            self.swap()
+            self.swap();
+            self.call_on_received();
         }
         state
     }
 
     fn sink_count(&self) -> u32 {
-        // the expectation here is, that this count does not change often
+    // the expectation here is, that this count does not change often
         Arc::strong_count(&self.signal) as u32 -1
     }
 
@@ -96,4 +101,46 @@ impl<T:Send> Source<T> where T: Clone + Sync + Default {
         state
     }
 
+    pub fn on_received(&mut self, closure: impl FnMut(&T) + 'static) {
+        self.on_received = Some(Box::new(closure))
+    }
+
+    fn call_on_received(&mut self) {
+        if self.is_synced() {
+            match &mut self.on_received {
+                Some(callback) => callback(self.store.as_ref().expect("always some value")),
+                None => {}
+            }
+        }
+    }
+
 }
+
+#[test]
+fn assume_on_received_is_not_executed_if_not_synced() {
+    use crate::signal;
+    // create a signal
+    let (mut src, snk) = signal::create();
+    let snk2 = Sink::from(&src);
+    src.send(&1);
+    src.on_received(|_| panic!()); // shall never be called, as signal is not synced
+    snk.receive();
+    snk2.receive();
+    src.send(&2); // send will call on_received, if set and signal is synced
+}
+
+#[test]
+fn assume_on_received_provides_expected_value() {
+    use crate::signal;
+    // create a synced signal
+    let (mut src, snk) = signal::create_synced();
+    let snk2 = Sink::from(&src);
+    src.on_received(|value| assert_eq!(*value, 0)); // assert i32 default, which is 0
+    // send will call on_received, if set and signal is synced
+    src.send(&1); // should give 0 in on_received, which is call in send
+    src.on_received(|value| assert_eq!(*value, 1)); // overwrite, and expect 1 for next call
+    snk.receive();
+    snk2.receive();
+    src.send(&2); // should give 1 in on_received
+}
+
