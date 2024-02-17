62c1c08cb3c623497d9811dd04590bc2b460c2ad
Peter Hahlweg <peter.github@mailbox.org>
2024-02-17 12:32:52 +0100

    Add changed function to sink
    - add also some missing doc on connect

--- src/signal/sink.rs
+++ src/signal/sink.rs
@@ -1,8 +1,10 @@
-use super::{*, loom::Arc};
+use super::{*, loom::{Arc, atomic::AtomicU64}};
+use std::sync::atomic::Ordering;
 
 // Sink
 pub struct Sink<T> where T: Clone + Sync + Send + Default {
     signal: Arc<Signal<T>>,
+    last_id: AtomicU64
 }
 
 
@@ -13,13 +15,14 @@ impl<T> Sink<T> where T: Clone + Sync + Send + Default {
     /// This is the only way to create a Sink. That's necessary to guaranty that both share the
     /// same sync property.
     pub fn from(source: &Source<T>) -> Self {
-        Sink { signal: source.signal() }
+        Sink { signal: source.signal(), last_id: AtomicU64::from(0) }
     }
 
     /// Returns a copy of the received signal value.
     /// This is especially useful for small or primitive types. If the signal data is to expansive
     /// to copy have a look at [process].
     pub fn receive(&self) -> T {
+        self.last_id.store(self.signal.box_id(), Ordering::Release);
         self.signal.value().0
     }
 
@@ -28,18 +31,25 @@ impl<T> Sink<T> where T: Clone + Sync + Send + Default {
     /// creates back pressure onto the sender if processing takes to much time (even if not
     /// synced).
     pub fn process(&self, closure: &mut dyn FnMut(&T)) {
+        self.last_id.store(self.signal.box_id(), Ordering::Release);
         self.signal.process(closure);
     }
 
+    /// Check if sink is connected.
     pub fn is_connected(&self) -> bool {
         Arc::strong_count(&self.signal) > 0
+    }
+
+    /// Check if source have changed the signal, since last acknowledge.
+    pub fn changed(&self) -> bool {
+       self.signal.box_id() == self.last_id.load(Ordering::Acquire)
     }
 
 }
 
 impl<T> Clone for Sink<T> where T: Clone + Sync + Send + Default {
     fn clone(&self) -> Self {
-        Sink { signal: self.signal.clone() }
+        Sink { signal: self.signal.clone(), last_id: AtomicU64::default()}
     }
 }
 
